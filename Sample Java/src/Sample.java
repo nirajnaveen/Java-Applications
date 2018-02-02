d61af5142346b322e46462c4049a76610a5fd054
MINGW64 <mingw64@live.in>
2018-02-02 19:48:10 +0530

    Second Commit

---
+++ Sample Java/src/Sample.java
@@ -0,0 +1,15 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello This is my First java program.");
+		System.out.println("This is second line of program");
+		System.out.println("Hello Let's Go....");
+		System.out.println("Hello man How are You!!");
+
+	 }
+  }
+
+
