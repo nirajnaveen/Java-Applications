ee9d746731f334da23dc4f222ee8c8b4387e2398
MINGW64 <mingw64@live.in>
2018-02-02 19:37:32 +0530

    First Commit Uploading All Files

---
+++ compare two string/src/EqualCheck.java
@@ -0,0 +1,25 @@
+ 
+public class EqualCheck{
+
+	public static void main(String[] args) {
+		// string defined
+      String a = "AVATAR";
+      String b = "avatar";
+      
+      if(a.equals(b)) {
+    	  System.out.println("Both strings are equal.");
+      }else {
+    	  System.out.println("Both string are not equal.");
+      }
+      
+      if(a.equalsIgnoreCase(b)) {
+    	  System.out.println("both string are equal");
+      }else {
+    	  System.out.println("Both trings are not equal.");
+      
+      }
+      
+      }
+	}
+
+
